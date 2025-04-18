4cfa8aa1ed95bd56c77527f058389970002a8533
Kowsikan <bgkv07kowsik2001@gmail.com>
2025-04-18 11:45:39 +0530

    Fix path to Excel file for GitHub Actions

--- tests/DataDriven/readExcel.js
+++ tests/DataDriven/readExcel.js
@@ -2,7 +2,7 @@ const XLSX = require("xlsx");
 const path = require("path");
 
 function readExcel() {
-    const filePath = path.resolve(__dirname, "../test-data/AjensiaLoginData.xlsx");
+    const filePath = path.resolve(__dirname, "./AjensiaLoginData.xlsx");
     const workbook = XLSX.readFile(filePath);
     const sheet = workbook.Sheets["Ajensia"];
     return XLSX.utils.sheet_to_json(sheet); // Convert the sheet to JSON
