f8addd226ff4218786817fe06c516a8f1157f828
Kowsikan <bgkv07kowsik2001@gmail.com>
2025-04-30 10:33:38 +0530

    rearrange

--- playwright.config.js
+++ playwright.config.js
@@ -1,31 +1,22 @@
+// @ts-check
 import { defineConfig, devices } from '@playwright/test';
-
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['allure-playwright'],
   ],
-
   timeout: 120_000,
-
-  
-
   use: {
     actionTimeout: 10_000,
     navigationTimeout: 20_000,
-    trace: 'on-first-retry',		 // Trace recording
-    video: 'retain-on-failure', 		// Video recording	
-    screenshot:  'only-on-failure', 	// Screenshot recording
+    trace: 'on-first-retry',
   },
-
-
   projects: [
     {
       name: 'chromium',
