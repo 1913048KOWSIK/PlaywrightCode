6d33a327b907bfccd6e3b271cf2ef1ad96bd3006
Kowsikan <bgkv07kowsik2001@gmail.com>
2025-04-30 11:23:48 +0530

    add Asset list 3

--- playwright.config.js
+++ playwright.config.js
@@ -1,30 +1,22 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  // Directory where test files are located
   testDir: './tests',
 
-  // Run tests in parallel
   fullyParallel: true,
 
-  // Fail build on CI if test.only is left accidentally
   forbidOnly: !!process.env.CI,
 
-  // Retry tests on CI
   retries: process.env.CI ? 2 : 0,
 
-  // Limit workers on CI to save resources
   workers: process.env.CI ? 1 : undefined,
 
-  // Reporters: list for CLI, html for artifact, allure for integration
   reporter: [
     ['list'],
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['allure-playwright'],
   ],
 
-  // Global timeouts
   timeout: 120_000,
 
   use: {
@@ -36,20 +28,12 @@ export default defineConfig({
   },
 
 
-  // Define projects for different browsers
+
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    // Uncomment to enable Firefox or WebKit
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    
   ],
 });
